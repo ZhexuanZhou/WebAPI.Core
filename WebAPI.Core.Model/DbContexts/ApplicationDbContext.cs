54591878a1c82c5559d3de14e74fac5178835855
zhexuanzhou <rmt_zhou@live.com>
2020-06-25 16:53:55 +0800

    User DbContext

---
+++ WebAPI.Core.Model/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Core.Model.Models;
+
+namespace WebAPI.Core.Model.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<AppUser> AppUser { get; set; }
+        public DbSet<AppRole> AppRole { get; set; }
+    }
+}
---
+++ WebAPI.Core.Model/Models/AppUserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Core.Model.Models
+{
+    public class AppUserRole : IdentityUserRole<int>
+    {
+        public bool? IsDeleted { get; set; }
+        public int? CreateId { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public int? ModifyId { get; set; }
+        public string ModifyBy { get; set; }
+        public DateTime? ModifyTime { get; set; }
+    }
+}
